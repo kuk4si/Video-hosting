9814a7c0e476345e61d135b1adad8656a3027cb7
Андрей Андрей <KUKASI222@outlook.com>
2022-06-30 18:44:49 +0200

    added video view

--- videos/urls.py
+++ videos/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import MainPage
+from .views import MainPage, VideoView
 
 urlpatterns = [
     path('', MainPage.as_view(), name='index'),
+    path('watch/<int:pk>', VideoView.as_view(), name='watch'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Video
 
 
 class MainPage(ListView):
     model = Video
     template_name = 'videos/main.html'
+
+
+class VideoView(DetailView):
+    model = Video
+    template_name = 'videos/video_view.html'
